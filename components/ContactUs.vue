5009646acb5b8b8c4a870cde48afe0c207ad1ba0
@Somsila <somsila99@gmail.com>
2025-01-29 12:53:11 +0700

    update contactus

---
+++ components/ContactUs.vue
@@ -0,0 +1,28 @@
+<script></script>
+
+<template>
+    <div id="contact" class="bg-gray-200 w-full h-14"></div>
+
+    <section
+        class="flex flex-col lg:flex-row items-center justify-between w-full h-full p-6 sm:p-12 lg:px-48 py-16 bg-gray-200">
+        <!-- Let Design Together -->
+        <div class="flex-1 space-y-4 mb-8 lg:mb-0 text-center lg:text-left">
+            <div
+                class="elementB_T flex flex-row lg:flex-col text-4xl md:text-5xl lg:text-6xl font-bold space-x-2 lg:space-x-0 lg:space-y-2 text-green-500">
+                <h1 class="text-center lg:text-left">Let</h1>
+                <h1 class="text-center lg:text-left">Design</h1>
+                <h1 class="text-center lg:text-left">Together</h1>
+            </div>
+
+            <p class="text-gray-600 max-w-lg mx-auto lg:mx-0 lg:pr-16 text-justify lg:text-left">
+                Collaborate to create unique, functional, and beautiful websites. Together, we’ll
+                bring your ideas to life with personalized design solutions.
+            </p>
+        </div>
+
+        <!-- Form Section -->
+        <div class="elementB_T w-full lg:w-auto flex justify-center">
+            <FormSubmit />
+        </div>
+    </section>
+</template>
