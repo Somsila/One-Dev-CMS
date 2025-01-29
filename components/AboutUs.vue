fd6aca4dcbe36ecc7d7c04bbe9a857087749adfb
@Somsila <somsila99@gmail.com>
2025-01-29 12:52:58 +0700

    update aboutus

---
+++ components/AboutUs.vue
@@ -0,0 +1,34 @@
+<script></script>
+<template>
+    <section
+    id="aboutus"
+    class="flex flex-wrap items-center justify-between w-full h-full px-6 py-10 bg-gray-200 lg:px-28"
+  >
+    <div
+      class="elementB_T w-full lg:w-1/2 flex justify-center relative mb-6 lg:mb-0"
+    >
+      <div class="relative w-full h-auto z-10 sm:w-72 lg:w-full">
+        <AboutUs-svg />
+      </div>
+    </div>
+
+    <div
+      class="elementB_T w-full lg:w-1/2 space-y-6"
+    >
+      <h1
+        class="text-3xl font-bold pb-2 sm:text-4xl lg:text-5xl lg:text-left sm:text-center md: text-center"
+      >
+        Discover Us
+      </h1>
+      <p class="text-gray-500 max-w-full pb-2 sm:max-w-md text-justify lg:text-left">
+        Welcome to <b class="text-green-500">One.Dev</b>, where passion drives innovation.
+        From the start, we've been dedicated to providing outstanding services that make a
+        real difference in people’s lives. We focus on quality, creativity, and putting
+        our customers first. At <b class="text-green-500">One.Dev</b>, we believe in
+        building strong relationships based on trust and continually adapting to meet your
+        needs. Join us in shaping a future of excellence and success together.
+      </p>
+    </div>
+    <div id="project" class="bg-gray-200 w-full h-14"></div>
+  </section>
+</template>
